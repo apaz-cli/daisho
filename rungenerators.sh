2ca9a4562b73e62c2eb2728a67892c10dfdc792b
apaz-cli <aarpazdera@gmail.com>
2021-10-05 15:39:34 -0500

    Updated rungenerators to fall back to system python.

--- rungenerators.sh
+++ rungenerators.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 cd src/Declarations/Generated
-python3 GenNodeTypes.py
-python3 GenTokTypes.py
+
+if ! command -v python3  &>/dev/null
+then
+	echo "Could not find python3. Falling back to system python, which may be Python2."
+	python GenNodeTypes.py
+	python GenTokTypes.py
+else
+	python3 GenNodeTypes.py
+	python3 GenTokTypes.py
+fi
