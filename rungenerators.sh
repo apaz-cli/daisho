555a82ac76249b0093fc8127ef48f3374cc66725
apaz-cli <aarpazdera@gmail.com>
2021-10-06 00:19:36 -0500

    Added testing framework.

--- rungenerators.sh
+++ rungenerators.sh
@@ -2,9 +2,8 @@
 
 cd src/Declarations/Generated
 
-if ! command -v python3  &>/dev/null
+if ! command -v python3 >/dev/null
 then
-	echo "Could not find python3. Falling back to system python, which may be Python2, resulting in a syntax error." 1>&2
 	python GenNodeTypes.py
 	python GenTokTypes.py
 else
---
+++ tests/test.py
@@ -0,0 +1,38 @@
+#!/bin/python
+#from subprocess import run
+
+import os
+from os import chdir as cd
+from glob import glob
+
+def pwd(): return print(os.getcwd)
+def run(s): print(s); os.system(s)
+def mkdir(s): pass
+
+
+def build():
+    # Build stiltc
+    cd('..')
+    run('./rungenerators.sh')
+    print("Building the compiler.")
+    run('./install.sh')
+
+    # Build the test scripts
+    cd('tests/')
+    for file in glob('scripts/*.c'):
+        fname = file[len('scripts/'):len(file)-2]
+        out_loc = f"bin/{fname}"
+        run(f'cc {file} -o {out_loc}')
+
+
+def runTests():
+    pass
+
+
+def cleanTests():
+    pass
+
+
+build()
+runTests()
+cleanTests()
