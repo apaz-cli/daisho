9302b12301a938601ab485ed6e916769493bdfbc
apaz-cli <aarpazdera@gmail.com>
2021-11-04 00:03:04 -0500

    .

--- stilts-stdlib/Native/StiltsString.h
+++ stilts-stdlib/Native/StiltsString.h
@@ -1,25 +1,27 @@
 #ifndef STILTS_STDLIB_STRING
 #define STILTS_STDLIB_STRING
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-#ifndef STD_STR_CHARBUF_LEN
-#ifndef STD_STR_CHARBUF_LEN 23
-#endif
+typedef struct {
+  char *buffer;
+  size_t size;
+  size_t _cap;
+} LargeStiltsStr;
 
-struct StiltsString {
-    char  ssopt[STD_STR_CHARBUF_LEN];
-    uchar remaining;
-};
-typedef struct StiltsString StiltsString;
+#define STILTS_SMALL_STR_CHARBUF_SIZE (sizeof(LargeStiltsStr) - 1)
 
+typedef struct {
+  char ssopt[STILTS_SMALL_STR_CHARBUF_SIZE];
+  char remaining;
+} SmallStiltsStr;
 
-
-
+typedef union {
+  SmallStiltsStr small;
+  LargeStiltsStr large;
+} StiltsString;
 
 #endif /* STILTS_STDLIB_STRING */
-
-
-
-
