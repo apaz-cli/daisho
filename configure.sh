28fa4c819eb289271ef32d294703b20a5978e864
apaz-cli <aarpazdera@gmail.com>
2021-12-29 00:59:25 -0600

    ANSI color config file.

--- configure.sh
+++ configure.sh
@@ -6,8 +6,42 @@
 CONFIG=""
 WRITE_TO="stdlib/Native/StiltsGeneratedConfig.h"
 COLS=$(expr $(stty size | cut -d' ' -f2) - 23)
+if test $COLS -gt 90; then COLS=57; fi
 NL=$(printf %s '\n')
-msg() { printf "%-20s: %"$COLS"s\n" "$1" "$2"; }
+
+# COLORS
+ncolors=$(tput colors)
+if test -n "$ncolors" && test "$ncolors" -ge 8; then
+    hascolors=1
+    bold="$(tput bold)"
+    underline="$(tput smul)"
+    standout="$(tput smso)"
+    normal="$(tput sgr0)"
+    black="$(tput setaf 0)"
+    red="$(tput setaf 1)"
+    green="$(tput setaf 2)"
+    yellow="$(tput setaf 3)"
+    blue="$(tput setaf 4)"
+    magenta="$(tput setaf 5)"
+    cyan="$(tput setaf 6)"
+    white="$(tput setaf 7)"
+else
+    hascolors=0
+    bold=""
+    underline=""
+    standout=""
+    normal=""
+    black=""
+    red=""
+    green=""
+    yellow=""
+    blue=""
+    magenta=""
+    cyan=""
+    white=""
+fi
+
+msg() { printf "${green}%-20s:${normal} ${yellow}%"$COLS"s${normal}\n" "$1" "$2"; }
 append() { CONFIG="$CONFIG$1$(printf %s '\n')"; }
 guard() { CONFIG="#pragma once$NL#ifndef __STILTS_STDLIB_GENERATEDCONFIG$NL#define __STILTS_STDLIB_GENERATEDCONFIG$NL$NL$CONFIG$NL#endif"; }
 writeconfig() { echo $CONFIG > $WRITE_TO; }
@@ -27,10 +61,20 @@ if sh ./config/endianness.sh; then :; else
 fi
 
 
-echo "###########################"
-echo "# Configuration Variables #"
-echo "###########################"
+echo $magenta"###########################"
+echo         "# Configuration Variables #"
+echo         "###########################"$normal
 
+##########
+# COLORS #
+##########
+if test $hascolors -eq 1; then
+    msg "ANSI COLORS" "YES"
+    append "#define __STILTS_HAS_ANSI_COLORS 1"
+else
+    msg "ANSI COLORS" "NO"
+    append "#define __STILTS_HAS_ANSI_COLORS 1"
+fi
 
 ##########
 # PYTHON #
