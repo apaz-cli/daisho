4cdb2039205ec9ce9b9148523505bc549350eebb
apaz-cli <aarpazdera@gmail.com>
2022-02-25 15:25:40 -0600

    Added murmurhash mix and djb2 string hashes.

---
+++ stdlib/Native/Builtins/Hash.h
@@ -0,0 +1,45 @@
+#ifndef __DAI_STDLIB_HASH
+#define __DAI_STDLIB_HASH
+
+
+__DAI_FN uint32_t
+__Dai_murm_fmi32(uint32_t h) {
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+    return h;
+}
+
+
+__DAI_FN uint64_t
+__Dai_murm_fmi64(uint64_t h) {
+    h ^= h >> 33;
+    h *= 0xff51afd7ed558ccdL;
+    h ^= h >> 33;
+    h *= 0xc4ceb9fe1a85ec53L;
+    h ^= h >> 33;
+    return h;
+}
+
+__DAI_FN uint32_t
+__Dai_strhash32(String str) {
+    char c; // djb2
+    uint32_t h = 5381;
+    while ((c = *str++))
+        h = ((h << 5) + h) + c;
+    return h;
+}
+
+__DAI_FN uint64_t
+__Dai_strhash64(String str) {
+    char c; // djb2
+    uint64_t h = 5381;
+    while ((c = *str++))
+        h = ((h << 5) + h) + c;
+    return h;
+}
+
+
+#endif /* __DAI_STDLIB_HASH */
