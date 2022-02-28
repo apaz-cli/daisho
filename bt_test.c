599987b50a772082451fac8a871e60573e1977ea
apaz-cli <aarpazdera@gmail.com>
2022-02-28 16:06:47 -0600

    Added to backtrace test. Doing addr2line stuff.

--- bt_test.c
+++ bt_test.c
@@ -13,6 +13,12 @@ typedef struct {
     char* addr;
 } __Dai_SymInfo;
 
+typedef struct {
+    void* frames;
+    int size;
+    int fd;
+} __Dai_fallback_args;
+
 __Dai_SymInfo
 __Dai_SymInfo_parse(char* str) {
     char *file = str, *name = NULL, *addr;
@@ -45,16 +51,85 @@ __Dai_SymInfo_parse(char* str) {
     return info;
 }
 
+// Handle failure in addr2line translation.
+void backtrace_fallback(__Dai_fallback_args args) {
+    backtrace_symbols_fd(args.frames, args.size, args.fd);
+    const char msg[] = "Failed to translate addresses to lines.";
+}
+
+// Returns 0 on failure, otherwise num written.
+// Call addr2line, format the results, and append to buf.
+size_t translate_append(char* buf, __Dai_fallback_args args, __Dai_SymInfo info) {
+    pid_t addrpid;
+    int status;
+
+    int pipes[2];
+    if (pipe(pipes) == -1) {
+        backtrace_fallback(args);
+        return 0;
+    }
+
+    pid = fork();
+    if (!pid) {
+        // Child
+
+        // Redirect stdout to pipe.
+        close(pipes[0]);
+        dup2(pipes[1], STDOUT_FILENO);
+
+	// Replace execution image or fail.
+        const char addrpath[] = "/usr/bin/addr2line";
+        const char addrname[] = "addr2line";
+        const char* addr2lineargs[] = { addrname, "-e", info.file, "-Cfpi", addr, NULL };
+        execv(addrpath, addr2lineargs);
+
+        backtrace_fallback(args);
+        return 0;
+    } else {
+        // Parent
+
+        // Wait for addr2line to write to pipe.
+        close(pipes[1]);
+        wait(&status);
+        int failure = (WIFEXITED(status) && WEXITSTATUS(status)) | WIFSIGNALED(status);
+        if (failure) {
+            backtrace_fallback(args);
+            return 0;
+        }
+
+        // Read from the pipe (addr2line child stdout).
+        char tmp[__DAI_BT_BUF_CAP];
+        ssize_t bytes_read = read(pipes[0], tmp, __DAI_BT_BUF_CAP);
+        if (bytes_read <= 0) {
+            backtrace_fallback(args);
+            return 0;
+        }
+        if (bytes_read == __DAI_BT_BUF_CAP) {
+            /* Large return probably doesn't matter. It shouldn't come up.
+               We can do multiple reads instead of erroring if it becomes a problem. */
+            backtrace_fallback(args);
+            return 0;
+        }
+
+        // Parse line, write to buf. Example of format:
+        // f1 at /home/apaz/git/Daisho/tests/scripts/backtrace_test.c:8
+        char* t = tmp;
+        
+    }
+
+    return 0;
+}
+
+#define __DAI_BT_MAX_FRAMES 50
 #define __DAI_BT_BUF_CAP (4096 * 4)
 static int fd;
 
-int
-init() {
+int init() {
     // Call backtrace once to load the
     // library so dlopen(), which calls malloc(),
     // is not called inside the signal handler.
-    void* frames[50];
-    int num_frames = backtrace(frames, 50);
+    void* frames[__DAI_BT_MAX_FRAMES];
+    int num_frames = backtrace(frames, __DAI_BT_MAX_FRAMES);
     if (!num_frames) return 0;
 
     // Create a temp file buffer.
@@ -70,10 +145,11 @@ main() {
     if (!init()) puts("Failed to initialize.");
 
     // Call backtrace.
-    void* frames[50];
-    int num_frames = backtrace(frames, 50);
+    __Dai_SymInfo frameinfo[__DAI_BT_MAX_FRAMES];
+    void* frames[__DAI_BT_MAX_FRAMES];
+    int num_frames = backtrace(frames, __DAI_BT_MAX_FRAMES);
     if (!num_frames) puts("No backtrace.");
-    if (num_frames > 50) puts("too long.");
+    if (num_frames > __DAI_BT_MAX_FRAMES) puts("too long.");
 
     // Take a backtrace, write it to the buffer.
     backtrace_symbols_fd(frames, num_frames, fd);
@@ -94,12 +170,13 @@ main() {
         while ((*next != '\n')) next++;
         next++;
 
-        __Dai_SymInfo info = __Dai_SymInfo_parse(str);
-        fprintf(stderr, "%s %s %s\n", info.file, info.name, info.addr);
+        frameinfo[n] = __Dai_SymInfo_parse(str);
+        fprintf(stderr, "%s %s %s\n", frameinfo[n].file, frameinfo[n].name, frameinfo[n].addr);
 
         str = next;
     }
 
     close(fd);
     puts("");
+
 }
