aaa78035b39976124b0d9df783762883d442192c
apaz-cli <aarpazdera@gmail.com>
2022-03-14 20:19:22 -0500

    Install script now only installs headers.

--- install.sh
+++ install.sh
@@ -1,22 +1,7 @@
 #!/bin/sh
 
-# COMPILE DAISHO
-FLAGS="-lpthread"
-if [ $1 ] && [ $1 = "release" ]; then
-  FLAGS="${FLAGS} -O3 -march=native"
-elif [ $1 ] && [ $1 = "aggressive-optimizations" ]; then
-  FLAGS="${FLAGS} -O3 -march=native -DAPAZ_HANDLE_UNLIKELY_ERRORS=0"
-elif [ $1 ] && [ $1 = "memdebug" ]; then
-  FLAGS="${FLAGS} -Og -g -rdynamic -fsanitize=address -DMEMDEBUG=1"
-else
-  FLAGS="${FLAGS} -O0 -g -rdynamic -fsanitize=address"
-fi
-
 # Move common headers into place
 sudo rm -rf /usr/include/daisho/
 sudo cp -r stdlib/ /usr/include/daisho/
 
-# Install daic executable
-cc daic/Compiler.c $FLAGS
-sudo mv a.out /usr/bin/daic
 
