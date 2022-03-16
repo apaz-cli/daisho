309b0002ebea3ea8574d0db7389c400b2ce0cc38
apaz-cli <aarpazdera@gmail.com>
2022-03-15 22:13:10 -0500

    Added support for en_US.UTF-8 locale.

--- config/locale.c
+++ config/locale.c
@@ -1,9 +1,15 @@
 #include <locale.h>
 #include <stdio.h>
 
-int main(void) {
+int
+main(void) {
     if (!setlocale(LC_ALL, "C.UTF-8")) {
-        fprintf(stderr, "Could not set locale to utf8.\n");
-        return 1;
+        if (!setlocale(LC_ALL, "en_US.UTF-8")) {
+            fprintf(stderr, "Could not set locale to utf8.\n");
+            return 1;
+        }
+        return 0;
+    } else {
+        return 0;
     }
 }
