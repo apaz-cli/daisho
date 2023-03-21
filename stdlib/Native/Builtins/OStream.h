ea87fdae1637303051fc651d95f1a05aed092d09
apaz-cli <aarpazdera@gmail.com>
2023-03-21 16:07:59 -0500

    Fixed write wrapper.

--- stdlib/Native/Builtins/OStream.h
+++ stdlib/Native/Builtins/OStream.h
@@ -24,16 +24,39 @@ static const _Dai_OStream* _Dai_stdout = &_Dai_stdout_;
 static const _Dai_OStream* _Dai_stderr = &_Dai_stderr_;
 
 _DAI_FN size_t
-_Dai_write_wrapper(int fd, const void* mem, size_t bytes) {
+_Dai_write_wrapper(int fd, const void* mem, size_t bytes, int* err) {
     size_t written_sofar = 0;
     while (bytes) {
         ssize_t bytes_written = write(fd, (char*)mem + written_sofar, bytes);
         if (bytes_written == -1 && errno == EINTR) continue;
-        _DAI_SANE_ASSERT(bytes_written != -1, "Write error");
+        if (bytes_written == -1 && errno != EINTR) return *err = errno, written_sofar;
         bytes -= bytes_written;
         written_sofar += bytes_written;
     }
     return written_sofar;
+}
+
+_DAI_FN size_t
+_Dai_read_wrapper(int fd, void* buf, size_t bytes, int* err) {
+    size_t have_read = 0;
+    while (1) {
+        ssize_t n = read(fd, (char*)buf + have_read, bytes - have_read);
+        if (n == -1) {
+            int e = errno;
+            if (e == EINTR)
+                continue;
+            else {
+                *err = e;
+                return have_read;
+            }
+        } else {
+            have_read += (size_t)n;
+            if (have_read >= bytes) break;
+        }
+    }
+
+    *err = 0;
+    return have_read;
 }
 
 _DAI_FN int
